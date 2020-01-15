60752dcd382824966fe9e06232236a759244ad5b
dong <dongxueqiang008@qq.com>
2020-01-15 22:34:59 +0800

    wsdl修改iptv名称空间，直接iptv命名空间，和项目名称无关1

---
+++ src/test/java/com/example/axis2demo/Axis2demoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.axis2demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Axis2demoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
