acf44a8a279a471b0a2efcbd729ab17ade62165b
dong <dongxueqiang008@qq.com>
2020-01-16 00:21:07 +0800

    wsdl修改iptv名称空间，直接iptv命名空间，和项目名称无关1,打包成war包

--- src/main/java/com/example/axis2demo/Axis2demoApplication.java
+++ src/main/java/com/example/axis2demo/Axis2demoApplication.java
@@ -2,12 +2,17 @@ package com.example.axis2demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Axis2demoApplication {
+public class Axis2demoApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(Axis2demoApplication.class, args);
     }
-
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Axis2demoApplication.class);
+    }
 }
